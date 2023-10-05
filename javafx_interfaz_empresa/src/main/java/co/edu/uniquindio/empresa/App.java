cdb84bdd3e2cba5e885d7f8725492468e7d58c84
Your Name <you@example.com>
2023-10-05 11:48:20 -0500

    Fondo de prueba implementado !Leer imports!

--- javafx_interfaz_empresa/src/main/java/co/edu/uniquindio/empresa/App.java
+++ javafx_interfaz_empresa/src/main/java/co/edu/uniquindio/empresa/App.java
@@ -1,12 +1,20 @@
 package co.edu.uniquindio.empresa;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App
@@ -17,7 +25,23 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        // Crear un contenedor raíz (por ejemplo, BorderPane)
+        Parent root = loadFXML("primary");
+
+        // Crear un Pane y establecer la imagen de fondo
+        Pane pane = new Pane();
+        Image backgroundImage = new Image("file:/Users/macbook/Desktop/Camilo/Repositorios/Interfaz_Empresa/Interfaz_Empresa/javafx_interfaz_empresa/Imágenes/background_empresa.png"); // Cambiar por la ruta de la imagen de acuerdo al computador de cada uno
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background backgroundWithImage = new Background(background);
+        pane.setBackground(backgroundWithImage);
+
+        // Agregar el contenedor raíz y el Pane con la imagen de fondo al StackPane
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().addAll(pane, root);
+
+        // Configurar la escena
+        scene = new Scene(stackPane, 1920, 1080);
         stage.setScene(scene);
         stage.show();
     }
@@ -34,5 +58,4 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
