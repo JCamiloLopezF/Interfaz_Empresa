0759830dec80af0888d69781aab7d1e02a974cc7
Your Name <you@example.com>
2023-10-13 21:03:47 -0500

    Ventana con menú y color creada

--- javafx_interfaz_empresa/src/main/java/co/edu/uniquindio/empresa/App.java
+++ javafx_interfaz_empresa/src/main/java/co/edu/uniquindio/empresa/App.java
@@ -1,61 +1,63 @@
 package co.edu.uniquindio.empresa;
 
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
-    public void start(Stage stage) throws IOException {
-        // Crear un contenedor raíz (por ejemplo, BorderPane)
-        Parent root = loadFXML("primary");
+    public void start(Stage welcomeUiStage) { //Crear la ventana
+        // Crear el contenedor o la raíz de toda la ventana
+        BorderPane raiz = new BorderPane();
 
-        // Crear un Pane y establecer la imagen de fondo
-        Pane pane = new Pane();
-        Image backgroundImage = new Image("file:/Users/macbook/Desktop/Camilo/Repositorios/Interfaz_Empresa/Interfaz_Empresa/javafx_interfaz_empresa/Imágenes/background_empresa.png"); // Cambiar por la ruta de la imagen de acuerdo al computador de cada uno
-        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
-        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
-        Background backgroundWithImage = new Background(background);
-        pane.setBackground(backgroundWithImage);
+        // Crear el fondo
+        BackgroundFill backgroundFill = new BackgroundFill(Color.web("#CCCCFF"), null, null);
+        Background background = new Background(backgroundFill);
+        raiz.setBackground(background);
 
-        // Agregar el contenedor raíz y el Pane con la imagen de fondo al StackPane
-        StackPane stackPane = new StackPane();
-        stackPane.getChildren().addAll(pane, root);
+        Scene scene = new Scene(raiz, 1920, 1080);
 
-        // Configurar la escena
-        scene = new Scene(stackPane, 1920, 1080);
-        stage.setScene(scene);
-        stage.show();
+        //Crea la barra de menú
+        MenuBar barraDeMenu = new MenuBar();
+        Menu gestionarEmpleadosItem = new Menu("Gestionar Empleados");
+        Menu gestionarProductosItem = new Menu("Gestionar Productos");
+        Menu gestionarClientesItem = new Menu("Gestionar Clientes");
+        Menu gestionarPrestamosItem = new Menu("Gestionar Préstamos");
+        barraDeMenu.getMenus().add(gestionarEmpleadosItem);
+        barraDeMenu.getMenus().add(gestionarPrestamosItem);
+        barraDeMenu.getMenus().add(gestionarProductosItem);
+        barraDeMenu.getMenus().add(gestionarClientesItem);
+
+
+        // Cambiar la fuente:
+        gestionarEmpleadosItem.setStyle("-fx-font-family:'Branding'");
+        gestionarPrestamosItem.setStyle("-fx-font-family: 'Branding'");
+        gestionarProductosItem.setStyle("-fx-font-family: 'Branding'");
+        gestionarClientesItem.setStyle("-fx-font-family: 'Branding'");
+
+        // Cambiar el color de la barra de menú
+        barraDeMenu.setStyle("-fx-background-color: #9999CC;");
+
+
+        // Colocar la barra de menú arriba
+        raiz.setTop(barraDeMenu);
+
+        welcomeUiStage.setScene(scene);
+        welcomeUiStage.setTitle("PréstamosBox");
+        welcomeUiStage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+    public static void main(String[] args) {// No sé qué hace, pero es importante, no lo borren pofavo'
+        launch(args);
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) {
-        launch();
+    public static void setRoot(String string) {//También no sé que hace, pero hace que corra el programa :)
     }
 }
