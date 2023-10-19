98153440e0c04b121de90dd7fa0334bcba6d2388
Your Name <you@example.com>
2023-10-19 00:03:08 -0500

    Cambios ConsultarCliente y creación de clase nueva
    Co-authored-by: Juan3bitas <Juan3bitas@users.noreply.github.com>
    Co-authored-by: HelenGiraldo <HelenGiraldo@users.noreply.github.com>
    Co-authored-by: Diego Flores <diegnghtmr011@users.noreply.github.com>

--- javafx_interfaz_empresa/src/main/java/co/edu/uniquindio/empresa/ConsultarEmpleado.java
+++ javafx_interfaz_empresa/src/main/java/co/edu/uniquindio/empresa/ConsultarEmpleado.java
@@ -3,9 +3,12 @@ package co.edu.uniquindio.empresa;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -18,6 +21,23 @@ public class ConsultarEmpleado extends Application {
         BackgroundFill backgroundFill = new BackgroundFill(Color.web("#CCCCFF"), null, null);
         Background background = new Background(backgroundFill);
         raiz.setBackground(background);
+
+        TextField codigoEmpleado = new TextField();
+        codigoEmpleado.setPromptText("Ingrese el código del empleado");
+
+        Label etiquetaCodigoEmpleado = new Label("Código del empleado");
+        etiquetaCodigoEmpleado.setTranslateY(-200);
+
+        VBox contenedorVBoxCodigoEmpleado = new VBox();
+        contenedorVBoxCodigoEmpleado.getChildren().addAll(codigoEmpleado);
+        contenedorVBoxCodigoEmpleado.setTranslateY(-200);
+
+        Button consultarEmpleado = new Button("Consultar Empleado");
+
+        consultarEmpleado.setOnAction(event ->{
+            MostrarEmpleadoConsultado mostrarEmpleadoWindow = new MostrarEmpleadoConsultado();
+            mostrarEmpleadoWindow.start(consultarEmpleadoWindow);
+        });
 
         Button volver = new Button("<--");
         volver.setMaxWidth(40);
---
+++ javafx_interfaz_empresa/src/main/java/co/edu/uniquindio/empresa/MostrarEmpleadoConsultado.java
@@ -0,0 +1,23 @@
+package co.edu.uniquindio.empresa;
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class MostrarEmpleadoConsultado extends Application {
+
+    @Override
+    public void start(Stage mostrarEmpleadoConsultadoWindow) {
+        BorderPane raiz = new BorderPane();
+
+        BackgroundFill backgroundFill = new BackgroundFill(Color.web("#CCCCFF"), null, null);
+        Background background = new Background(backgroundFill);
+        raiz.setBackground(background);
+
+        Label mostrarEmpleadoConsultado = new Label("El empleado que buscó es: ");
+        mostrarEmpleadoConsultado.setTranslateY(-200);
+    }
+}
