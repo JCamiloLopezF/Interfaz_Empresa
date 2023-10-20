47c9b13f3df1b5684ed5f1f400161b7b6171f7d0
Your Name <you@example.com>
2023-10-20 00:09:55 -0500

    Cambios jueves 19-10-2023
    Co-authored-by: HelenGiraldo <HelenGiraldo@users.noreply.github.com>
    Co-authored-by: Diego Flores <diegnghtmr011@users.noreply.github.com>
    Co-authored-by: Juan3bitas <Juan3bitas@users.noreply.github.com>

--- javafx_interfaz_empresa/src/main/java/co/edu/uniquindio/empresa/ConsultarEmpleado.java
+++ javafx_interfaz_empresa/src/main/java/co/edu/uniquindio/empresa/ConsultarEmpleado.java
@@ -2,13 +2,15 @@ package co.edu.uniquindio.empresa;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -28,10 +30,6 @@ public class ConsultarEmpleado extends Application {
         Label etiquetaCodigoEmpleado = new Label("Código del empleado");
         etiquetaCodigoEmpleado.setTranslateY(-200);
 
-        VBox contenedorVBoxCodigoEmpleado = new VBox();
-        contenedorVBoxCodigoEmpleado.getChildren().addAll(codigoEmpleado);
-        contenedorVBoxCodigoEmpleado.setTranslateY(-200);
-
         Button consultarEmpleado = new Button("Buscar");
 
         consultarEmpleado.setOnAction(event ->{
@@ -43,16 +41,58 @@ public class ConsultarEmpleado extends Application {
         volver.setMaxWidth(40);
         raiz.setTop(volver);
 
+        Label numeroDeDocumentoEmpleadoLabel = new Label("Ingrese el número de documento del empleado");
+        numeroDeDocumentoEmpleadoLabel.setTranslateY(-90);
+        TextField numeroDeDocumentoEmpleado = new TextField();
+        numeroDeDocumentoEmpleado.setPromptText("Ingrese el número de documento del empleado");
+        numeroDeDocumentoEmpleado.setTranslateY(-50);
+
+        Button buscar = new Button("Buscar");
+
+        buscar.setOnAction(event -> {
+            boolean numeroDocumentoValido = !numeroDeDocumentoEmpleado.getText().isEmpty();
+            switch (evaluarCampos(numeroDocumentoValido)){
+                case "ÉXITO":
+                MostrarEmpleadoConsultado mostrarEmpleadoConsultadoWindow = new MostrarEmpleadoConsultado();
+                mostrarEmpleadoConsultadoWindow.start(consultarEmpleadoWindow);
+                break;
+                case "ERROR_DOCUMENTO":
+                mostrarAdvertencia("¡Este campo es obligatorio!", "El campo de nombre del objeto es un campo obligatorio");
+        }
+        });
+
+        StackPane contenedorStackPane = new StackPane();
+        contenedorStackPane.getChildren().addAll(numeroDeDocumentoEmpleado, buscar, numeroDeDocumentoEmpleadoLabel);
+        contenedorStackPane.setMaxWidth(270);
+
+        raiz.setCenter(contenedorStackPane);
+
         volver.setOnAction(event -> {
             App welcomeUiStage = new App();
             welcomeUiStage.start(consultarEmpleadoWindow);
         });
-
-
+ 
         Scene scene = new Scene(raiz, 1920, 780);
 
         consultarEmpleadoWindow.setScene(scene);
         consultarEmpleadoWindow.setTitle("Gestionar Empleados: Consultar Empleado");
         consultarEmpleadoWindow.show();
     }
+    
+        private void mostrarAdvertencia(String titulo, String mensaje) {
+        Alert alert = new Alert(AlertType.WARNING);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    private String evaluarCampos(boolean numeroDocumentoValido){
+        if (numeroDocumentoValido){
+            return "ÉXITO";}// Todos los campos llenos
+        else if (!numeroDocumentoValido){
+            return "ERROR_DOCUMENTO";}//Nombre vacío
+
+        return "ERROR ERUJ-43897";//Para cubrir otros tipos de errores no contemplados en los else if
+        }
 }
